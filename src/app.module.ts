8a5471a1ea43a2e7406fcc8b75e0602aba702558
Jorjikia Beka <jorjikiabeka@Jorjikias-MacBook-Air.local>
2023-11-17 18:55:21 +0400

    connected to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
+  imports: [ ConfigModule.forRoot({
+    envFilePath: '.env',
+    isGlobal: true, 
+  }),
+  MongooseModule.forRoot(process.env.DB_URI),],
   controllers: [AppController],
   providers: [AppService],
 })
